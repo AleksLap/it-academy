0239614800aad668e0745dcd8e385adb4701589b
Aleksandr Lapanovich <a.lapanovich@gmail.com>
2020-01-23 17:04:38 +0300

    Update main.js

--- DomHomeWork for 23.01/main.js
+++ DomHomeWork for 23.01/main.js
@@ -25,19 +25,24 @@ initEvenHandles();
 console.log(htmlElements);
 
 function selectColorValueChanged() {
-  setColor(htmlElements.selectColor.value);
-
   // 1. Вызывает setColor, передает знчение выпадающего списка в качестве входного параметра
+  setColor(htmlElements.selectColor.value);
 }
 
 function addButtonClicked() {
+  // 1. Вызывает checkIfColorCanBeAdded, чтобы проверить, существует ли название цвета, которое пользователь ввел в текстовом поле, или нет.
+  // 1.1. Если такого названия цвета не существует, показывает сообщение ""There's no such a color"".
+  // 1.2. Если же такое название цвета существует, вызывает checkIfColorAdded, чтобы проверить наличие добавляемого цвета выпадающем списке.
+  // 1.2.1. Если выпадающий список еще не содержит добавляемое значение, последовательно вызывает функции addColor, chooseColor и setColor, передавая им в качестве входного параметра значение, которое пользователь ввел в текстовое поле.
+  // 1.2.2. Если значение уже добавлено, показывает сообщение 'Color has been already added'
+
   let color = htmlElements.input.value;
   let checkColor = checkIfColorCanBeAdded(color);
 
-  if(checkColor === true){
+  if(checkColor){
     let checkIfAdded = checkIfColorAdded(color);
 
-    if(checkIfAdded === true){
+    if(checkIfAdded){
       htmlElements.input.style.borderColor = 'red';
       alert('Color has been already added');
     }
@@ -51,15 +56,14 @@ function addButtonClicked() {
     htmlElements.input.style.borderColor = 'red';
     alert('There is no such a color');
   }
-
-  // 1. Вызывает checkIfColorCanBeAdded, чтобы проверить, существует ли название цвета, которое пользователь ввел в текстовом поле, или нет.
-  // 1.1. Если такого названия цвета не существует, показывает сообщение ""There's no such a color"".
-  // 1.2. Если же такое название цвета существует, вызывает checkIfColorAdded, чтобы проверить наличие добавляемого цвета выпадающем списке.
-  // 1.2.1. Если выпадающий список еще не содержит добавляемое значение, последовательно вызывает функции addColor, chooseColor и setColor, передавая им в качестве входного параметра значение, которое пользователь ввел в текстовое поле.
-  // 1.2.2. Если значение уже добавлено, показывает сообщение 'Color has been already added'
 }
 
 function removeColorClicked() {
+  // 1. Проверяет, ввел ли пользователь какой-то текст в текстовое поле
+  // 1.1. Если не ввел, показывает сообщение 'Please enter a value first'
+  // 1.2. Если ввел, вызвает removeColor, передавая значение текстового поля в качестве входного параметра.
+  // После чего вызывает reset.
+
   let color = htmlElements.input.value;
 
   if(color.length < 1){
@@ -69,13 +73,13 @@ function removeColorClicked() {
     removeColor(color);
     reset();
   }
-
-  // 1. Проверяет, ввел ли пользователь какой-то текст в текстовое поле
-  // 1.1. Если не ввел, показывает сообщение 'Please enter a value first'
-  // 1.2. Если ввел, вызвает removeColor, передавая значение текстового поля в качестве входного параметра. После чего вызывает reset.
 }
 
 function removeSelectedColorClicked() {
+  // 1. Вызывает getSelectedValue, чтобы получить значение выбранного option в выпадающем списке.
+  // 1.1. Если значение выбранного option равно 'not selected', показывает сообщение 'Please choose a value to remove'.
+  // 1.2. Если же значение выбранного option не равно 'not selected', вызывает removeColor, передавая значение выбранного option в качестве входного параметра.
+
   let value = getSelectedValue();
 
   if(value === 'not selected'){
@@ -85,22 +89,18 @@ function removeSelectedColorClicked() {
     removeColor(value);
     reset();
   }
-
-  // 1. Вызывает getSelectedValue, чтобы получить значение выбранного option в выпадающем списке.
-  // 1.1. Если значение выбранного option равно 'not selected', показывает сообщение 'Please choose a value to remove'.
-  // 1.2. Если же значение выбранного option не равно 'not selected', вызывает removeColor, передавая значение выбранного option в качестве входного параметра.
 }
 
 function getSelectedValue() {
+  // 1. находит HTML элемент по селектору
+  // 2. возвращает найденный HTML элемент
+
   let selectedIdx = htmlElements.selectColor.selectedIndex;
   return htmlElements.selectColor[selectedIdx].value;
 }
 
 function getHTMLElement(selector) {
   return document.querySelector(selector);
-
-  // 1. находит HTML элемент по селектору
-  // 2. возвращает найденный HTML элемент
 }
 
 function checkIfColorAdded(color) {
@@ -115,27 +115,26 @@ function checkIfColorAdded(color) {
 }
 
 function addColor(color) {
-  let option = new Option(color, color);
-  htmlElements.selectColor.appendChild(option);
-  reset();
-
   // 1. Создает новый HTML элмент option через new Option (https://mzl.la/createOption)
   // 2. Вызывает appendChild (https://mzl.la/2J1CTEo) на выпадающем списке, указывая созданный элмент option в качестве входящего параметра
   // 3. Вызывает функцию reset
+
+  let option = new Option(color, color);
+  htmlElements.selectColor.appendChild(option);
+  reset();
 }
 
 function chooseColor(color) {
+  // 1. Так же, как и в функции checkIfColorAdded, пробегает по всем option выпадающего списка начиная со второго элемента (начальное условие цикла будет let i = 1, т.к.
+  // элементы коллекций в JavaScript считаются с 0, а нулевой option имеет значение -- Select a value --, поэтому мы не учитываем его в расчетах)
+  // 1.1. Как только i-тое значение одного из option совпало со значением цвета, указанного в качестве входного параметра, вызывает функцию setSelectedIndex, передавая в нее индекс i итератора цикла.
 
   for (let i = 1; i < htmlElements.selectColor.length; i++) {
 
     if (htmlElements.selectColor[i].value === color) {
-      setSelectedIndex(i-1);
+      setSelectedIndex(i);
     }
   }
-
-  // 1. Так же, как и в функции checkIfColorAdded, пробегает по всем option выпадающего списка начиная со второго элемента (начальное условие цикла будет let i = 1, т.к.
-  // элементы коллекций в JavaScript считаются с 0, а нулевой option имеет значение -- Select a value --, поэтому мы не учитываем его в расчетах)
-  // 1.1. Как только i-тое значение одного из option совпало со значением цвета, указанного в качестве входного параметра, вызывает функцию setSelectedIndex, передавая в нее индекс i итератора цикла.
 }
 
 function setSelectedIndex(idx) {
@@ -143,15 +142,15 @@ function setSelectedIndex(idx) {
 }
 
 function setColor(color) {
-  htmlElements.body.style.setProperty('--color', color);
-
   // меняет цвет у body через CSS переменную --color
+
+  htmlElements.body.style.setProperty('--color', color);
 }
 
 function removeColor(color) {
   let colorAdded = checkIfColorAdded(color);
 
-  if (colorAdded === true) {
+  if (colorAdded) {
     for (let i = 1; i < htmlElements.selectColor.length; i++) {
       if (htmlElements.selectColor[i].value === color) {
         htmlElements.selectColor.removeChild(htmlElements.selectColor[i]);
@@ -164,12 +163,12 @@ function removeColor(color) {
 }
 
 function reset() {
+  // 1. Устанавливает значение текстового поля в ''
+  // 2. Удаляет CSS переменную --color на body
+
   htmlElements.input.value = '';
   htmlElements.body.style.setProperty('--color', '');
   htmlElements.input.style.borderColor = '';
-
-  // 1. Устанавливает значение текстового поля в ''
-  // 2. Удаляет CSS переменную --color на body
 }
 
 function checkIfColorCanBeAdded(color) {
@@ -189,7 +188,7 @@ function redBorderNotification() {
 let inputValue = htmlElements.input.value;
 let check = checkIfColorCanBeAdded(inputValue);
 
-if(check === false){
+if(!check){
   htmlElements.input.style.borderColor = 'red';
 }
 if(inputValue.length < 1){
